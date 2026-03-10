06f1c75765877c4dfa36198a2e67fdc308df363f
vedikakesharwani52328-debug <vedikakesharwani52328@gmail.com>
2026-03-10 22:20:23 +0530

    Add files via upload

---
+++ UseCase4.java
@@ -0,0 +1,17 @@
+public class UseCase4 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "  ***     ***    *****   ***** ",
+            " *   *   *   *   *    *  *     ",
+            " *   *   *   *   *****   ***** ",
+            " *   *   *   *   *           *  ",
+            "  ***     ***    *       ***** "
+        };
+
+        for (String line : lines) {   // small 'l'
+            System.out.println(line);
+        }
+    }
+}
